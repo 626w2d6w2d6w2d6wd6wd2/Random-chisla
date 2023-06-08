afe2d3fbcd75813969576a4f355d91504fe8b59a
Серега626w2d6w2d6w2d6wd6wd2 <seregakrivolap@gmail.com>
2023-06-08 16:13:33 +0300

    515

---
+++ Src/MyApp.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class MyApp {
+   public static void main(String[]args){
+       int[]array =new int[100];
+       int lenghtAraay = array.length;
+
+       System.out.println(lenghtAraay);
+       System.out.println(Arrays.toString(array));
+       for (int i=1;i<array.length;i++){
+           System.out.println(i);
+       }
+
+
+   }
+
+
+       }
+
