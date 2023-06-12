00550bfef78d9b3424764b067b25a739cb5e05cc
Серега626w2d6w2d6w2d6wd6wd2 <seregakrivolap@gmail.com>
2023-06-12 14:21:40 +0300

    454545

--- Src/MyApp.java
+++ Src/MyApp.java
@@ -2,19 +2,21 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class MyApp {
-   public static void main(String[]args){
-       int[]array =new int[100];
-       int lenghtAraay = array.length;
+   public static void main(String[]args) {
 
-       System.out.println(lenghtAraay);
-       System.out.println(Arrays.toString(array));
-       for (int i=1;i<array.length;i++){
-           System.out.println(i);
+       int[] arr = new int[94];
+
+       for (int i = 0, j = 1; j <= 100; j++) {
+           int d1 = j % 10;
+           int d2 = j / 10;
+           if (d1 != 4 && d2 != 4 && d1 != 9 && d2 != 9) {
+               arr[i++] = j;
+           }
        }
-
-
+       System.out.println(Arrays.toString(arr));
    }
+}
 
 
-       }
+
 
